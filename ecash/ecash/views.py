5677ae3007a536b8cd253a7ac446f8bf997d263a
mbugua <jobugua97@gmail.com>
2023-07-26 12:18:34 +0300

    test commit

--- ecash/ecash/views.py
+++ ecash/ecash/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, JsonResponse
 import requests
 
 
-mdb_endpoint=' https://18cb-105-48-225-20.ngrok-free.app/getdata'
+mdb_endpoint=' http://18cb-105-48-225-20.ngrok-free.app/getdata'
 
 
 def callback(request):
