58aba54d31ed9eab2dd91b0496dff6920f3cef9f
mbugua <jobugua97@gmail.com>
2023-07-26 11:21:10 +0300

    test commit

--- ecash/ecash/views.py
+++ ecash/ecash/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render,HttpResponse
+from django.http import HttpResponse, JsonResponse
+import requests
+
+
+mdb_endpoint=' https://18cb-105-48-225-20.ngrok-free.app/getdata'
+
 
 def callback(request):
-    return HttpResponse("<h1> this is a test from versel</h1>")
+    data=request.body.decode('\n','')
+    dbdata={"data":data}
+    if not data is None:
+        try:
+           re=requests.post(mdb_endpoint,data=dbdata)
+        except:
+            pass    
+    response = {
+		'ResultCode': 0,
+		'ResultDesc': 'Accepted'
+	}
+    return JsonResponse(response)
