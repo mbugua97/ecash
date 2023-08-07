87b0e36cf158f367c7695a60eb44b2ea36b19e70
mbugua <jobugua97@gmail.com>
2023-08-07 21:18:45 +0300

    new commits

--- ecash/ecash/views.py
+++ ecash/ecash/views.py
@@ -7,18 +7,12 @@ mdb_endpoint='https://0a48-105-161-158-238.ngrok-free.app/getdata'
 
 
 def callback(request):
-    try:
      data=request.body.decode('\n','')
      dbdata={"data":data}
-    except:
-     dbdata={"data":'message failed data from vers'}
-    if not data is None:
-        try:
-           re=requests.post(mdb_endpoint,data=dbdata)
-        except:
-            pass    
-    response = {
+      
+     re=requests.post(mdb_endpoint,data=dbdata)    
+     response = {
 		'ResultCode': 0,
 		'ResultDesc': 'Accepted'
 	}
-    return JsonResponse(response)
+     return JsonResponse(response)
